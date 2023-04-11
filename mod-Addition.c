722a7ae137ea99389b005324f7c25f4d332f8e0e
Ben Collins <mbakrnechukwucollins@gmail.com>
2023-04-11 21:58:28 +0100

    addition calculator

--- mod-Addition.c
+++ mod-Addition.c
@@ -1,5 +1,37 @@
+/**
+  * intro - Creating an addition calculator
+  *
+  */
 #include "main.h"
 
 /**
- * main - A function that adds numbers
- */
+  * main - A function that adds numbers
+  *
+  * Return: Always 0
+  */
+
+int main(void)
+{
+	float sum = 0;
+	float num[15] = {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0};
+	int i = 0;
+	int k = 0;
+
+	printf("HELLO AND WELCOME TO MY ADDITION CALCULATOR\n\n");
+	printf("---------------------------------------------\n");
+
+	printf("Input The Numbers: ");
+
+/**
+  * Accepts the numbers
+  *
+  */
+
+	for (i = 0; i > 2; i++)
+	{
+		scanf("%f", &num[i]);
+	}
+/**
+  * Calculates the sum of the numbers
+  */
+}
