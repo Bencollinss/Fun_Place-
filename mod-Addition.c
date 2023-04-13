31d84b98892055a0b5289ac06b3d1580e58cf62e
Ben Collins <mbakrnechukwucollins@gmail.com>
2023-04-13 21:32:57 +0100

    finishing touches

--- mod-Addition.c
+++ mod-Addition.c
@@ -13,30 +13,33 @@
 int main(void)
 {
 	float sum = 0;
-	float num[15] = {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0};
+	float num[2] = {0,0};
 	int i = 0;
 	int k = 0;
 
 	printf("HELLO AND WELCOME TO MY ADDITION CALCULATOR\n\n");
 	printf("---------------------------------------------\n");
 
-	printf("Input The Numbers: ");
+	printf("Input The Numbers you want to add: ");
 
 /**
   * Accepts the numbers
   *
   */
 
-	for (i = 0; i > 2; i++)
+	for (i = 0; i < 2; i++)
 	{
 		scanf("%f", &num[i]);
 	}
 /**
   * Calculates the sum of the numbers
   */
-	for (k = 0; k < 15; k++)
+	for (k = 0; k < 2; k++)
 	{
-		
+		sum = sum + num[k];
 	}
-}
 
+	printf("The sum of your numbers is [%f]\n", sum);
+
+	return (0);
+}
